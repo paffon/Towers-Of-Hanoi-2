cfed80bcbad7313ec4dc198b369ca05c98a2febe
Omri <paffon@gmail.com>
2021-04-23 18:29:17 +0200

    refactor imports

--- Towers-of-Hanoi-2/src/main/main.java
+++ Towers-of-Hanoi-2/src/main/main.java
@@ -5,7 +5,10 @@ import TowersOfHanoi.TowersOfHanoi;
 import java.util.Scanner;
 
 public class main {
+
     public static void main(String[] args) {
+        System.out.println("Hello world");
+
         Scanner sc = new Scanner(System.in);
 
         int discsAmount = getUserInput("How many discs?", sc);
